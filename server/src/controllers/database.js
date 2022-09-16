79c26b2bb5fbfc405f0d26898fe3687cc90f92fb
Edward Abboud <abboudedward@gmail.com>
2022-09-16 17:01:04 +0200

    successful sign up form

--- server/src/controllers/database.js
+++ server/src/controllers/database.js
@@ -1,11 +1,16 @@
 import Statement from "../models/Statement.js";
+import User from "../models/User.js";
 import { logError } from "../util/logging.js";
 
 export const countStatements = async (req, res) => {
   try {
     const total = await Statement.countDocuments({});
+    const totalUsers = await User.countDocuments({});
+
     const result = {
+      type: "database",
       numOfTotalStatements: total,
+      numOfTotalUsers: totalUsers,
     };
 
     res.status(200).json({ success: true, result: result });
