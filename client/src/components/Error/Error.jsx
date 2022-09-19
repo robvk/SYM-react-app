d6064198e1d9363de68002d578497d38b96cca7c
Edward Abboud <abboudedward@gmail.com>
2022-09-19 22:41:37 +0200

    adjusted the erro component to fit within the forms further testing will be needed where other components are concerned

--- client/src/components/Error/Error.jsx
+++ client/src/components/Error/Error.jsx
@@ -1,17 +1,17 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styles from "./Error.module.css";
+import appStyle from "../../App.module.css";
 
 const Error = ({ error }) => {
   return (
     <section className={styles.container}>
-      {/* <header className={styles.header}>
-        <h3 className={styles.title}>Oops! An error occured!</h3>
-      </header> */}
-      <div className={styles.body}>
-        <p>
-          <strong>{error.toString()}</strong>
-        </p>
+      <div className={appStyle.body}>
+        {error && (
+          <p>
+            <strong>{error.toString()}</strong>
+          </p>
+        )}
       </div>
     </section>
   );
