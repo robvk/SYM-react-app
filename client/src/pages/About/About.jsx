934c322f8248dcafba839769c4aa19e0659f6f05
Edward Abboud <abboudedward@gmail.com>
2022-10-22 22:47:37 +0200

    adjusted about page css

--- client/src/pages/About/About.jsx
+++ client/src/pages/About/About.jsx
@@ -44,110 +44,123 @@ const About = () => {
         <div className={style.bgImageContainer}>
           <img src={bgImage} className={style.bgImage} />
         </div>
-        <div className={style.aboutSection}>
-          <div className={style.container}>
-            <h2 className={appStyle.headerOne}>About SYM</h2>
-            <div className={style.row}>
-              <div className={style.titleContainer}>
-                <p className={appStyle.headerTwo}>
-                  Make a statement, the world is waiting..
-                </p>
-              </div>
-              <div className={style.paragraphContainer}>
-                <p className={appStyle.body}>
-                  SYM aims to restore anonymity in social interaction platforms.
-                  The idea is to let anyone open topics for discussion without
-                  having to explain themselves, hence the character limit. What
-                  you do here is open a discussion with one sentence and make a
-                  statement with another. Any SYM user is then able to reply to
-                  your topic by tagging a statement to your topic.
-                  <br />
-                  <br />
-                  Users cannot follow other users. Each statement awards the
-                  user points that add up to the SYM score. Posts are sorted by
-                  most liked. Each post will self destruct in 48h. When a user
-                  tags on a statement they also get points to their SYM score.
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-        {error ? (
-          <Error error={error} />
-        ) : (
-          <div className={style.statsSection}>
-            <div className={style.content}>
-              <p className={appStyle.headerOne}>Users</p>
-              <div className={style.numbers}>
-                <CountUp className={appStyle.headerOne} start={0} end={users} />
-              </div>
-            </div>
-            <div className={style.content}>
-              <p className={appStyle.headerOne}>Active Statements</p>
-              <div className={style.numbers}>
-                <CountUp
-                  className={appStyle.headerOne}
-                  start={0}
-                  end={activeStatements}
-                />
-              </div>
-            </div>
-            <div className={style.content}>
-              <p className={appStyle.headerOne}>Active Tags</p>
-              <div className={style.numbers}>
-                <CountUp className={appStyle.headerOne} start={0} end={tags} />
-              </div>
-            </div>
-            <div className={style.content}>
-              <p className={appStyle.headerOne}>Expired Posts</p>
-              <div className={style.numbers}>
-                <CountUp
-                  className={appStyle.headerOne}
-                  start={0}
-                  end={expiredStatements}
-                />
-              </div>
-            </div>
-            <div className={style.content}>
-              <p className={appStyle.headerOne}>Expired Tags</p>
-              <div className={style.numbers}>
-                <CountUp
-                  className={appStyle.headerOne}
-                  start={0}
-                  end={expiredComments}
-                />
+        <div className={style.aboutContent}>
+          <div className={style.aboutSection}>
+            <div className={style.container}>
+              <h2 className={appStyle.headerOne}>About SYM</h2>
+              <div className={style.row}>
+                <div className={style.titleContainer}>
+                  <p className={appStyle.headerTwo}>
+                    Make a statement, the world is waiting..
+                  </p>
+                </div>
+                <div className={style.paragraphContainer}>
+                  <p className={appStyle.body}>
+                    SYM aims to restore anonymity in social interaction
+                    platforms. The idea is to let anyone open topics for
+                    discussion without having to explain themselves, hence the
+                    character limit. What you do here is open a discussion with
+                    one sentence and make a statement with another. Any SYM user
+                    is then able to reply to your topic by tagging a statement
+                    to your topic.
+                    <br />
+                    <br />
+                    Users cannot follow other users. Each statement awards the
+                    user points that add up to the SYM score. Posts are sorted
+                    by most liked. Each post will self destruct in 48h. When a
+                    user tags on a statement they also get points to their SYM
+                    score.
+                  </p>
+                </div>
               </div>
             </div>
           </div>
-        )}
-        <div className={style.teamSection}>
-          <div className={style.title}>
-            <h3 className={appStyle.headerTwo}>The creative Team</h3>
-          </div>
-          <div className={style.content}>
-            <p className={appStyle.body}>
-              This SPA was developed by Edward Abboud as a portfolio project.
-              The repository for this app is public and inspired by a repository
-              from Hack Your Future&apos;s final project repository.
-              <br />
-              <br />
-              To learn more about the creator check out their{" "}
-              <a
-                target="_blank"
-                href="https://github.com/EdwardAbboud"
-                rel="noreferrer"
-              >
-                GitHub
-              </a>{" "}
-              or{" "}
-              <a
-                target="_blank"
-                href="https://www.linkedin.com/in/edward-abboud/"
-                rel="noreferrer"
-              >
-                LinkedIn
-              </a>
-            </p>
+          {error ? (
+            <Error error={error} />
+          ) : (
+            <div className={style.statsSection}>
+              <div className={style.content}>
+                <p className={appStyle.headerOne}>Users</p>
+                <div className={style.numbers}>
+                  <CountUp
+                    className={appStyle.headerOne}
+                    start={0}
+                    end={users}
+                  />
+                </div>
+              </div>
+              <div className={style.content}>
+                <p className={appStyle.headerOne}>Active Statements</p>
+                <div className={style.numbers}>
+                  <CountUp
+                    className={appStyle.headerOne}
+                    start={0}
+                    end={activeStatements}
+                  />
+                </div>
+              </div>
+              <div className={style.content}>
+                <p className={appStyle.headerOne}>Active Tags</p>
+                <div className={style.numbers}>
+                  <CountUp
+                    className={appStyle.headerOne}
+                    start={0}
+                    end={tags}
+                  />
+                </div>
+              </div>
+              <div className={style.content}>
+                <p className={appStyle.headerOne}>Expired Posts</p>
+                <div className={style.numbers}>
+                  <CountUp
+                    className={appStyle.headerOne}
+                    start={0}
+                    end={expiredStatements}
+                  />
+                </div>
+              </div>
+              <div className={style.content}>
+                <p className={appStyle.headerOne}>Expired Tags</p>
+                <div className={style.numbers}>
+                  <CountUp
+                    className={appStyle.headerOne}
+                    start={0}
+                    end={expiredComments}
+                  />
+                </div>
+              </div>
+            </div>
+          )}
+          <div className={style.teamSection}>
+            <div className={style.title}>
+              <h3 className={appStyle.headerTwo}>The creative Team</h3>
+            </div>
+            <div className={style.content}>
+              <p className={appStyle.body}>
+                This SPA was developed by Edward Abboud as a portfolio project.
+                The repository for this app is public and inspired by a
+                repository from Hack Your Future&apos;s final project
+                repository.
+                <br />
+                <br />
+                To learn more about the creator check out their{" "}
+                <a
+                  target="_blank"
+                  href="https://github.com/EdwardAbboud"
+                  rel="noreferrer"
+                >
+                  GitHub
+                </a>{" "}
+                or{" "}
+                <a
+                  target="_blank"
+                  href="https://www.linkedin.com/in/edward-abboud/"
+                  rel="noreferrer"
+                >
+                  LinkedIn
+                </a>
+              </p>
+            </div>
           </div>
         </div>
       </div>
